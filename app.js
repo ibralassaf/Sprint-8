b26d15a9cb4957f4eed51fc25c9ff3e54234ca07
Ibrahim Alassaf <xibra9141@gmail.com>
2022-04-20 20:47:16 +0300

    update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //originalStory 'In ${userInputs[0]}, computer pioneer ${userInputs[1]} found herself working on a ${userInputs[2]}. Word got out that the team had "debugged" the ${userInputs[2]}'
 
-let originalStory = "Once upon a time, there was a brave princess who...";
-
+//the story
+let originalStory = `In ${userInputs[0]}, computer pioneer ${userInputs[1]} found herself working on a ${userInputs[2]}. Word got out that the team had "debugged" the ${userInputs[2]}`;
+//array of user inputs
 let userInputs = [];
 
-let protagonistName = `In ${userInputs[0]}, computer pioneer ${userInputs[1]} found herself working on a ${userInputs[2]}. Word got out that the team had "debugged" the ${userInputs[2]}`;
-
+//Total question
 let numberOfQuestions = 3;
