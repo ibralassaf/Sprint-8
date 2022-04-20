acea3d2905f838430517ca45d89b89590d573260
Ibrahim Alassaf <xibra9141@gmail.com>
2022-04-20 20:30:53 +0300

    update app.js and index.html

---
+++ app.js
@@ -0,0 +1,7 @@
+//originalStory 'In ${userInputs[0]}, computer pioneer ${userInputs[1]} found herself working on a ${userInputs[2]}. Word got out that the team had "debugged" the ${userInputs[2]}'
+
+let originalStory = "Once upon a time, there was a brave princess who...";
+
+let userInputs = [];
+
+let protagonistName = `In ${userInputs[0]}, computer pioneer ${userInputs[1]} found herself working on a ${userInputs[2]}. Word got out that the team had "debugged" the ${userInputs[2]}`;
