668db849f8ec05af19d1a169664610ccc5cb936a
Ibrahim Alassaf <xibra9141@gmail.com>
2022-04-20 20:44:14 +0300

    Added declarations of mad-lib replacement variables

--- app.js
+++ app.js
@@ -5,3 +5,5 @@ let originalStory = "Once upon a time, there was a brave princess who...";
 let userInputs = [];
 
 let protagonistName = `In ${userInputs[0]}, computer pioneer ${userInputs[1]} found herself working on a ${userInputs[2]}. Word got out that the team had "debugged" the ${userInputs[2]}`;
+
+let numberOfQuestions = 3;
