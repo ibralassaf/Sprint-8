64a725df193fe3d808cbcb590446acf79ee161c9
Ibrahim Alassaf <xibra9141@gmail.com>
2022-04-20 20:52:06 +0300

    update app.js

--- app.js
+++ app.js
@@ -7,3 +7,16 @@ let userInputs = [];
 
 //Total question
 let numberOfQuestions = 3;
+
+//user-prompts for each mad-lib replacement, e.g. "Enter a verb", "Enter a noun", etc.
+let enterVerb = prompt("Enter a verb");
+let enterNoun = prompt("Enter a noun");
+let enterAdjective = prompt("Enter an adjective");
+
+//push user inputs into array
+let questionArray = [enterVerb, enterNoun, enterAdjective];
+
+//logic that will store the user inputs in the correct variables
+for (let i = questions; i >= 0; i--) {
+    console.log(i);
+}
