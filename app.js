fb61e7619b59a6d5a68ff08397f07a6bb8cba966
Ibrahim Alassaf <xibra9141@gmail.com>
2022-04-20 21:30:36 +0300

    update app.js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+"use strict";
+
 //originalStory 'In ${userInputs[0]}, computer pioneer ${userInputs[1]} found herself working on a ${userInputs[2]}. Word got out that the team had "debugged" the ${userInputs[2]}'
 
 //Total question
 let numberOfQuestions = 3;
+
+let questionCounter = 0;
 
 //user-prompts for each mad-lib replacement, e.g. "Enter a verb", "Enter a noun", etc.
 let enterVerb = prompt("Enter a verb");
@@ -13,12 +17,11 @@ let questionArray = [enterVerb, enterNoun, enterAdjective];
 
 //logic that will store the user inputs in the correct variables
 for (let i = numberOfQuestions; i >= 0; i--) {
-    console.log(questionArray[i]);
+    console.log(questionCounter++);
 }
-
-let questionCounter = 0;
 
 //the story
 let originalStory = `In ${userInputs[0]}, computer pioneer ${userInputs[1]} found herself working on a ${userInputs[2]}. Word got out that the team had "debugged" the ${userInputs[2]}`;
+
 //array of user inputs
 let userInputs = [];
