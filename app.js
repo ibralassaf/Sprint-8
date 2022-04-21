a3eb4cc802cae91a2f3c896ddd81b62e6156ef25
Ibrahim Alassaf <xibra9141@gmail.com>
2022-04-21 03:27:12 +0300

    update app.js

--- app.js
+++ app.js
@@ -7,31 +7,34 @@ let totalQuestion = 3;
 
 let questionCounter = 0;
 
-//user-prompts for each mad-lib replacement, e.g. "Enter a verb", "Enter a noun", etc.
-let enterName = prompt(
-    "What is your name: " + `; (${totalQuestion--} questions left)`
-);
-let enterHobby = prompt(
-    "What is your favorite hobby: " + `; (${totalQuestion--} questions left)`
-);
-let enterColor = prompt(
-    "What is your favorite color: " + `; (${totalQuestion--} questions left)`
-);
-
-//push user inputs into array
-let questionArray = [enterName, enterHobby, enterColor];
-
-//read the zero-th index (of your array) *first*, and *then* increase the counter).
-console.log(questionArray[0]);
-
-//logic that will store the user inputs in the correct variables
-for (let i = 2; i > 0; i--) {
-    questionCounter++;
-    console.log(questionArray[questionCounter]);
-}
-
-//the story
-let originalStory = `In ${userInputs[0]}, computer pioneer ${userInputs[1]} found herself working on a ${userInputs[2]}. Word got out that the team had "debugged" the ${userInputs[2]}`;
+//question array
+let questionArray = [
+    "Enter your full name",
+    "Enter your favorite hobby",
+    "Enter your favorite programming language",
+    "Enter your favorite food",
+];
 
 //array of user inputs
 let userInputs = [];
+//logic that will store the user inputs in the correct variables
+for (let i = totalQuestion; i >= 0; i--) {
+    console.log(
+        questionArray[questionCounter] + `... (${totalQuestion} questions left)`
+    );
+    userInputs.push(
+        prompt(
+            questionArray[questionCounter] + `... (${totalQuestion} questions left)`
+        )
+    );
+    console.log(`This is the current "questionCounter" : ${questionCounter}`);
+    console.log(userInputs);
+    questionCounter++;
+    totalQuestion--;
+}
+alert(
+    "All done! Ready for your totally-accurate, not-at-all confusing history of tech??"
+);
+
+//the story
+//let originalStory = `In ${userInputs[0]}, computer pioneer ${userInputs[1]} found herself working on a ${userInputs[2]}. Word got out that the team had "debugged" the ${userInputs[2]}`;
